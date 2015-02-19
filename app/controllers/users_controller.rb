b19b5298703de8252a11c62337ab574e8fedb466
JaredCowan <jaredlc@outlook.com>
2015-02-18 17:53:59 -0800

    testing fix again

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,6 @@
 class UsersController < ApplicationController
   load_and_authorize_resource
-  require 'net/http'
 
   def show
-    @user = User.find_by(first_name: params[:first_name])
-    if !@user
-      @user = User.find_by(id: params[:id])
-    end
   end
 end
